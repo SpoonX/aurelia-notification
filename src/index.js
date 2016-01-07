52a34f3976ea054b6470d809866c6d54e72be2c1
doktordirk <doktordirk@outlook.de>
2016-01-07 21:35:09 +0100

    chore(index): Linted

--- src/index.js
+++ src/index.js
@@ -5,23 +5,24 @@ import {Optional} from 'aurelia-dependency-injection';
 
 @inject(humane, new Optional(I18N))
 export class Notification {
-  constructor (humane, i18n) {
-    this.humane = humane;
+  constructor(_humane, i18n) {
+    this.humane = _humane;
     this.i18n   = i18n;
   }
 
-  _log(message, options) {
-    if (this.i18n && this.i18n.i18next.isInitialized())
+  log(message, options) {
+    if (this.i18n && this.i18n.i18next.isInitialized()) {
       message = this.i18n.tr(message);
+    }
 
-    this.humane.log(message, options); 
+    this.humane.log(message, options);
   }
 
-  success (message) {
-    this._log(message, {addnCls: 'success'});
+  success(message) {
+    this.log(message, {addnCls: 'success'});
   }
 
-  error (message) {
-    this._log(message, {addnCls: 'error'});
+  error(message) {
+    this.log(message, {addnCls: 'error'});
   }
 }
