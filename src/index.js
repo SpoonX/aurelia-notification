e172e5e4b9b325637b538e3c26cd081c40aaee98
doktordirk <doktordirk@outlook.de>
2016-01-07 19:43:14 +0100

    feat(i18n): I18n used only if that module is installed and initialized

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import humane from 'wavded/humane-js';
 import {inject} from 'aurelia-framework';
 import {I18N} from 'aurelia-i18n';
+import {Optional} from 'aurelia-dependency-injection';
 
-@inject(humane, I18N)
+@inject(humane, new Optional(I18N))
 export class Notification {
   constructor (humane, i18n) {
     this.humane = humane;
     this.i18n   = i18n;
   }
 
+  _log(message, options) {
+    if (this.i18n && this.i18n.i18next.isInitialized())
+      message = this.i18n.tr(message);
+
+    this.humane.log(message, options); 
+  }
+
   success (message) {
-    this.humane.log(this.i18n.tr(message), {addnCls: 'success'});
+    this._log(message, {addnCls: 'success'});
   }
 
   error (message) {
-    this.humane.log(this.i18n.tr(message), {addnCls: 'error'});
+    this._log(message, {addnCls: 'error'});
   }
 }
