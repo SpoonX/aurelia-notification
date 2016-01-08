4c225e7417b8b80e6a919d48d366df52aec93c6d
doktordirk <doktordirk@outlook.de>
2016-01-08 10:57:48 +0100

    feat(notification): Allow Array of Strings

--- src/index.js
+++ src/index.js
@@ -5,24 +5,69 @@ import {Optional} from 'aurelia-dependency-injection';
 
 @inject(humane, new Optional(I18N))
 export class Notification {
+  
+  /**
+   * Construct.
+   *
+   * @param {humane} humane
+   * @param {i18N} [I18N]
+   *
+   * @constructor
+   */
   constructor(_humane, i18n) {
-    this.humane = _humane;
+    this._humane = _humane;
     this.i18n   = i18n;
   }
 
+  /**
+   * Humane instance. See http://wavded.github.io/humane-js/
+   *
+   * @return {humane}
+   */
+  get humane() {
+    return this._humane;
+  }
+
+  /**
+   * Notify (translated if applicable) using Humane.log.
+   *
+   * @param {String|String[]}  message|multi-line message.
+   * @param {{}|function} [options|callback] passed on to humane.
+   *
+   */
   log(message, options) {
     if (this.i18n && this.i18n.i18next.isInitialized()) {
-      message = this.i18n.tr(message);
+      if (message instanceof Array) {
+        message = message.map(item=>this.i18n.tr(item));
+      } else {
+        message = this.i18n.tr(message);
+      }
     }
 
-    this.humane.log(message, options);
+    this._humane.log(message, options);
   }
 
-  success(message) {
-    this.log(message, {addnCls: 'success'});
+  /**
+   * Notify success (translated if applicable).
+   * {addnCls: 'success'} is automatically passed on to humane.
+   *
+   * @param {String|String[]}  message|multi-line message.
+   * @param {{}} [options] passed on to humane.
+   *
+   */
+  success(message, options = {}) {
+    this.log(message, Object.assign(options, {addnCls: 'success'});
   }
 
-  error(message) {
-    this.log(message, {addnCls: 'error'});
+  /**
+   * Notify error (translated if applicable).
+   * {addnCls: 'error'} is automatically passed on to humane.
+   *
+   * @param {String|String[]}  message|multi-line message.
+   * @param {{}} [options] passed on to humane.
+   *
+   */
+  error(message, options= {}) {
+    this.log(message, Object.assign(options, {addnCls: 'error'});
   }
 }
