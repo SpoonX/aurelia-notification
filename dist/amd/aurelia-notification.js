5baf4be0f97e81c7941f2672ea279b36bf6717e8
doktordirk <doktordirk@outlook.de>
2016-04-10 16:33:20 +0200

    chore(project): new build

--- dist/amd/aurelia-notification.js
+++ dist/amd/aurelia-notification.js
@@ -1,16 +1,184 @@
-define(['exports', './notification', './config'], function (exports, _notification, _config) {
+define(['exports', 'extend', 'humane-js', 'aurelia-dependency-injection', 'aurelia-i18n', 'javascript-decorators', 'aurelia-pal'], function (exports, _extend, _humaneJs, _aureliaDependencyInjection, _aureliaI18n, _javascriptDecorators, _aureliaPal) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-  exports.configure = exports.Notification = undefined;
+  exports.configure = exports.Notification = exports.Config = undefined;
+
+  var _extend2 = _interopRequireDefault(_extend);
+
+  var _humaneJs2 = _interopRequireDefault(_humaneJs);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+    var desc = {};
+    Object['ke' + 'ys'](descriptor).forEach(function (key) {
+      desc[key] = descriptor[key];
+    });
+    desc.enumerable = !!desc.enumerable;
+    desc.configurable = !!desc.configurable;
+
+    if ('value' in desc || desc.initializer) {
+      desc.writable = true;
+    }
+
+    desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+      return decorator(target, property, desc) || desc;
+    }, desc);
+
+    if (context && desc.initializer !== void 0) {
+      desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+      desc.initializer = undefined;
+    }
+
+    if (desc.initializer === void 0) {
+      Object['define' + 'Property'](target, property, desc);
+      desc = null;
+    }
+
+    return desc;
+  }
+
+  var _dec, _dec2, _dec3, _dec4, _dec5, _dec6, _dec7, _dec8, _class2, _desc, _value, _class3;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var Config = exports.Config = function () {
+    function Config() {
+      _classCallCheck(this, Config);
+
+      this.translate = true;
+      this.defaults = {};
+      this.notifications = {
+        note: {},
+        success: { addnCls: 'success' },
+        error: { addnCls: 'error' },
+        info: { addnCls: 'info' }
+      };
+    }
+
+    Config.prototype.configure = function configure() {
+      var incomming = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+      var base = arguments.length <= 1 || arguments[1] === undefined ? this : arguments[1];
+
+      this.translate = 'translate' in incomming ? incomming.translate : base.translate;
+      this.defaults = (0, _extend2.default)({}, base.defaults, incomming.defaults);
+      this.notifications = (0, _extend2.default)({}, base.notifications, incomming.notifications);
+
+      return this;
+    };
+
+    return Config;
+  }();
+
+  var Notification = exports.Notification = (_dec = (0, _aureliaDependencyInjection.inject)(Config, _humaneJs2.default, _aureliaI18n.I18N), _dec2 = (0, _javascriptDecorators.readonly)(), _dec3 = (0, _javascriptDecorators.readonly)(), _dec4 = (0, _javascriptDecorators.readonly)(), _dec5 = (0, _javascriptDecorators.readonly)(), _dec6 = (0, _javascriptDecorators.readonly)(), _dec7 = (0, _javascriptDecorators.readonly)(), _dec8 = (0, _javascriptDecorators.readonly)(), _dec(_class2 = (_class3 = function () {
+    function Notification(config, humane, i18n) {
+      var _this = this;
+
+      _classCallCheck(this, Notification);
+
+      this.define('__config', config).define('__humane', humane).define('__i18n', i18n);
+
+      this.setBaseCls();
+
+      for (var key in config.notifications) {
+        this[key] = this.spawn(config.notifications[key]);
+      }
+
+      this.setContainer();
+      var aureliaComposedListener = function aureliaComposedListener() {
+        _this.setContainer();
+        _aureliaPal.DOM.removeEventListener('aurelia-composed', aureliaComposedListener);
+      };
+      _aureliaPal.DOM.addEventListener('aurelia-composed', aureliaComposedListener);
+    }
+
+    Notification.prototype.define = function define(property, value, writable) {
+      Object.defineProperty(this, property, {
+        value: value,
+        writable: !!writable,
+        enumerable: false
+      });
+
+      return this;
+    };
+
+    Notification.prototype.setContainer = function setContainer(container) {
+      _aureliaPal.DOM.appendNode(this.__humane.el, container);
+      this.__humane.container = this.__humane.el.parentNode;
+      return this.__humane.container;
+    };
+
+    Notification.prototype.setBaseCls = function setBaseCls() {
+      var baseCls = arguments.length <= 0 || arguments[0] === undefined ? this.__config.defaults.baseCls : arguments[0];
+
+      this.__humane.baseCls = baseCls ? baseCls : this.__humane.baseCls;
+      return this.__humane.baseCls;
+    };
+
+    Notification.prototype.translate = function translate(options, defaults) {
+      var joined = (0, _extend2.default)({}, this.__config, defaults, options);
+      return joined.translate;
+    };
+
+    Notification.prototype.log = function log(message, options) {
+      var _this2 = this;
+
+      var defaults = arguments.length <= 2 || arguments[2] === undefined ? this.__config.defaults : arguments[2];
+
+      if (this.translate()) {
+        if (message instanceof Array) {
+          message = message.map(function (item) {
+            return _this2.i18n.tr(item);
+          });
+        } else {
+          message = this.__i18n.tr(message);
+        }
+      }
+
+      return new Promise(function (resolve) {
+        return _this2.__humane.log(message, options, resolve, defaults);
+      });
+    };
+
+    Notification.prototype.spawn = function spawn(addnDefaults) {
+      var _this3 = this;
+
+      addnDefaults = typeof addnDefaults === 'string' ? { 'addnCls': addnDefaults } : addnDefaults;
+      var defaults = (0, _extend2.default)({}, this.__config.defaults, addnDefaults);
+
+      return function (message, options) {
+        return _this3.log(message, options, defaults);
+      };
+    };
+
+    Notification.prototype.remove = function remove() {
+      var _this4 = this;
+
+      return new Promise(function (resolve) {
+        return _this4.__humane.remove(resolve);
+      });
+    };
+
+    return Notification;
+  }(), (_applyDecoratedDescriptor(_class3.prototype, 'define', [_dec2], Object.getOwnPropertyDescriptor(_class3.prototype, 'define'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'setContainer', [_dec3], Object.getOwnPropertyDescriptor(_class3.prototype, 'setContainer'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'setBaseCls', [_dec4], Object.getOwnPropertyDescriptor(_class3.prototype, 'setBaseCls'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'translate', [_dec5], Object.getOwnPropertyDescriptor(_class3.prototype, 'translate'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'log', [_dec6], Object.getOwnPropertyDescriptor(_class3.prototype, 'log'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'spawn', [_dec7], Object.getOwnPropertyDescriptor(_class3.prototype, 'spawn'), _class3.prototype), _applyDecoratedDescriptor(_class3.prototype, 'remove', [_dec8], Object.getOwnPropertyDescriptor(_class3.prototype, 'remove'), _class3.prototype)), _class3)) || _class2);
 
 
   function configure(aurelia, config) {
-    return config(aurelia.container.get(_config.Config));
+    return config(aurelia.container.get(Config));
   }
 
-  exports.Notification = _notification.Notification;
+  exports.Config = Config;
+  exports.Notification = Notification;
   exports.configure = configure;
 });
---
+++ dist/aurelia-notification.js
@@ -0,0 +1,195 @@
+import extend from 'extend';
+import Humane from 'humane-js';
+import {inject} from 'aurelia-dependency-injection';
+import {I18N} from 'aurelia-i18n';
+import {readonly} from 'javascript-decorators';
+import {DOM} from 'aurelia-pal';
+
+export class Config {
+  // translate on/off
+  translate = true
+  // defaults for all notifictaions
+  defaults = {}
+  // notification names and their specific defaults
+  notifications = {
+    note: {},
+    success: {addnCls: 'success'},
+    error: {addnCls: 'error'},
+    info: {addnCls: 'info'}
+  }
+
+  configure(incomming = {}, base = this) {
+    this.translate     = 'translate' in incomming ? incomming.translate : base.translate;
+    this.defaults      = extend({}, base.defaults, incomming.defaults);
+    this.notifications = extend({}, base.notifications, incomming.notifications);
+
+    return this;
+  }
+}
+
+@inject(Config, Humane, I18N)
+export class Notification {
+
+  /**
+   * Construct.
+   *
+   * @param {Config} config
+   * @param {Humane} humane
+   * @param {i18N}   I18N
+   *
+   * @constructor
+   */
+  constructor(config, humane, i18n) {
+    this
+      .define('__config', config)
+      .define('__humane', humane)
+      .define('__i18n', i18n);
+
+    // set baseCls
+    this.setBaseCls();
+
+    // add configured default methods
+    for (let key in config.notifications) {
+      this[key] = this.spawn(config.notifications[key]);
+    }
+
+    // ensure humane.container is document.body after 'aurelia-composed'
+    this.setContainer();
+    let aureliaComposedListener = () => {
+      this.setContainer();
+      DOM.removeEventListener('aurelia-composed', aureliaComposedListener);
+    };
+    DOM.addEventListener('aurelia-composed', aureliaComposedListener);
+  }
+
+  /**
+   * Define a non-enumerable property on the Notification.
+   *
+   * @param {string}  property
+   * @param {*}       value
+   * @param {boolean} [writable]
+   *
+   * @return {Notification}
+   */
+  @readonly()
+  define(property, value, writable) {
+    Object.defineProperty(this, property, {
+      value: value,
+      writable: !!writable,
+      enumerable: false
+    });
+
+    return this;
+  }
+
+  /**
+   * Set the container for the notifications
+   *
+   * @param {[DOM.node]}  [container] for the notifications
+   *
+   * @return {DOM.node}  [container]
+   *
+   */
+  @readonly()
+  setContainer(container) {
+    DOM.appendNode(this.__humane.el, container); // if container null or undefined,  appends to document.body
+    this.__humane.container = this.__humane.el.parentNode;
+    return this.__humane.container;
+  }
+
+  /**
+   * Set the base css class for the notifications
+   *
+   * @param {[string]}  [base class] for the notifications (default=__config.defaults.baseCls)
+   *
+   * @return {string}  [base class]
+   *
+   */
+  @readonly()
+  setBaseCls(baseCls = this.__config.defaults.baseCls) {
+    this.__humane.baseCls = baseCls ? baseCls : this.__humane.baseCls;
+    return this.__humane.baseCls;
+  }
+
+  /**
+   * Check if translate is on with given options
+   *
+   * @param {[{}]}  [options] for a particular notification.
+   * @param {[{}]}  [defaults] for a type of notifications.
+   *
+   * @return {Boolean}
+   *
+   */
+  @readonly()
+  translate(options, defaults) {
+    let joined = extend({}, this.__config, defaults, options);
+    return joined.translate;
+  }
+
+  /**
+   * Notify (translated if applicable) using humane.log.
+   *
+   * @param {String|String[]}  message|multi-line message.
+   * @param {{}}               [options] for this particular notification.
+   * @param {{}}               [defaults] for this type of notification.
+   *
+   * @return {Promise}
+   *
+   */
+  @readonly()
+  log(message, options, defaults = this.__config.defaults) {
+    if (this.translate()) {
+      if (message instanceof Array) {
+        message = message.map(item=>this.i18n.tr(item));
+      } else {
+        message = this.__i18n.tr(message);
+      }
+    }
+
+    return new Promise(resolve => {
+      return this.__humane.log(message, options, resolve, defaults);
+    });
+  }
+
+  /**
+   * Set a custom shortcut for .log with defaults based on global defaults
+   *
+   * @param {String|{}}  [defaults] for this shortcut.
+   *                     A string evaluates to {'addnCls': defaults}
+   *
+   * @return {function(message, options)}
+   *
+   */
+  @readonly()
+  spawn(addnDefaults) {
+    addnDefaults = (typeof addnDefaults === 'string') ? {'addnCls': addnDefaults} : addnDefaults;
+    let defaults = extend({}, this.__config.defaults, addnDefaults);
+
+    return (message, options) => {
+      return this.log(message, options, defaults);
+    };
+  }
+
+  /**
+   * Force remove humane log
+   *
+   * @return {Promise}
+   *
+   */
+  @readonly()
+  remove() {
+    return new Promise(resolve => {
+      return this.__humane.remove(resolve);
+    });
+  }
+}
+
+function configure(aurelia, config) {
+  return config(aurelia.container.get(Config));
+}
+
+export {
+  Config,
+  Notification,
+  configure
+};
